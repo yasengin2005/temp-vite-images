0afd3e12b07d333690c79ba67bde8900e6470386
enginbukum <yasengin2005@gmail.com>
2023-09-22 15:02:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+import SearchForm from "./SearchForm";
+import Gallery from "./Gallery";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
